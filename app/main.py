ba35d57d34ad05b1e763f7b2ecd39cc33f41a5f3
sunil chavare <sunilchavare1665@gmail.com>
2026-03-09 16:57:59 +0000

    register auth router in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.api.routes.users import router as users_router
-from app.api.routes.auth import 
+from app.api.routes.auth import router as auth_router
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Production API")
+
+app.include_router(auth_router, prefix="/api/v1", tags=["auth"])
 
 app.include_router(users_router, prefix="/api/v1", tags=["users"])
