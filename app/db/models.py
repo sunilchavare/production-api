c5e7d1b12f3b6bdba276f80b06ac95da3dab3fbc
sunil chavare <sunilchavare1665@gmail.com>
2026-03-09 16:51:06 +0000

    add hashed_password field to User model

--- app/db/models.py
+++ app/db/models.py
@@ -5,3 +5,4 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String,nullable=False)
